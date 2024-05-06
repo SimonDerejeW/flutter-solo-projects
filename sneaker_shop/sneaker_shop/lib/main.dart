89f99c982166c5cfdadde61bab557f6b37737a08
Simon <simondereje1@gmail.com>
2024-05-06 16:28:33 +0300

    Added Hive structure and changed default icon of the app into the nike logo

--- sneaker_shop/sneaker_shop/lib/main.dart
+++ sneaker_shop/sneaker_shop/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:provider/provider.dart';
 import 'package:sneaker_shop/models/cart.dart';
 import 'package:sneaker_shop/pages/home_page.dart';
 import 'package:sneaker_shop/pages/intro_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async{
+  //Initializing Hive
+  await Hive.initFlutter();
+
+  //Opening a hive box
+  var box = await Hive.openBox("mYBox");
+
+
+
   runApp(const MyApp());
 }
 
